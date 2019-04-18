34c9bff40e61bb3a06f09bc9793353f7e4fcb5cb
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-18 19:25:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,6 +71,11 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
+        
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,8 +89,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
